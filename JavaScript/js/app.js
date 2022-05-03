6211e01211a134cdc406344130767e43e1b3365c
Ahuitz Caracciolo <ahuitzcaracciolo@gmail.com>
2022-05-02 21:58:56 -0300

    agrego caprpeta con practicas de JavaScript

---
+++ JavaScript/js/app.js
@@ -0,0 +1,9 @@
+function CambiarColor() {
+    document.getElementById("OnMouseColor").style.backgroundColor ="red";
+    
+}
+
+function VueltaColor() {
+    document.getElementById("OnMouseColor").style.backgroundColor ="#aaa";
+    
+}
