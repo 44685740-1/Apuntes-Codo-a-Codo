4dfaf0f4cdcd34df9f37eba7d17b34911e463b0d
Ahuitz Caracciolo <ahuitzcaracciolo@gmail.com>
2022-07-16 12:26:05 -0300

    agrego dos archivos javascript

--- JavaScript/js/app.js
+++ JavaScript/js/app.js
@@ -7,3 +7,18 @@ function VueltaColor() {
     document.getElementById("OnMouseColor").style.backgroundColor ="#aaa";
     
 }
+
+function imprimir() {
+    window.print();
+}
+
+
+// hay que usar alt + 96 para las comillas que te permiten usar el $ y que imprima las variables
+
+function sumar() {
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+
+    var result = parseFloat(a) + parseFloat(b);
+    document.getElementById("ResultadoSuma").innerHTML = `<span style= 'color=red'> la resta de ${a} - ${b} = ${result}`;
+}
